e00769886682159842c166c91fddf48c2378e2e2
Joey Tran <duytran.arch@gmail.com>
2021-11-24 22:00:09 +0700

    update

--- auto_revit/view_writting.py
+++ auto_revit/view_writting.py
@@ -3,12 +3,14 @@ import sys
 import clr
 clr.AddReference('ProtoGeometry')
 from Autodesk.DesignScript.Geometry import *
+import Autodesk.DesignScript.Geometry.Point as pt
 clr.AddReference('RevitAPI')
 from Autodesk.Revit.DB import *
 clr.AddReference('RevitAPIUI')
 from Autodesk.Revit.UI import TaskDialog
 clr.AddReference("RevitNodes")
 import Revit
+import Revit.Elements.TextNote as tn
 clr.ImportExtensions(Revit.Elements)
 clr.AddReference("RevitServices")
 import RevitServices
@@ -30,12 +32,49 @@ def get_sheet_view_by_search_string(search_string):
 	    return view_
     except:
     	return
-
-
-
+def get_textnote(view_):
+	global doc , debugger,text_type
+	text_collector = list(FilteredElementCollector(doc,view_.Id).OfClass(TextNote))
+	dic_text = {}
+	for t in text_collector:
+		try:
+			# Global Text Type:
+			text_type = t.TextNoteType
+			dic_text[str(t.Id)] = {}
+			# GIÁ TRI TEXTNOTE		
+			dic_text[str(t.Id)]["TextNoteValue"] = t.Text.strip()#GetFormattedText().GetPlainText().strip()				
+			# GIÁ TRI TEXTNOTE LIST sep TAB
+			dic_text[str(t.Id)]["TextNoteValueColumnList"] = []			
+			dic_text[str(t.Id)]["TextNoteValueColumnList"].append(t.Text.strip().split("\t"))#GetFormattedText().GetPlainText().strip()	
+			# ÐO DAI TEXTNOTE		
+			text_type_border = t.TextNoteType.LookupParameter("Leader/Border Offset").AsDouble()*304.8			
+			dic_text[str(t.Id)]["TextNoteWidth"] = ("{:0.0f}".format(round(t.Width*304.8 + 2*text_type_border)))			
+			# TEXTNOTE TYPE
+			dic_text[str(t.Id)]["TextNoteType"] = t.Name#GetFormattedText().GetPlainText().strip()				
+			# TAB SIZE
+			dic_text[str(t.Id)]["TextNoteTabSize"] = t.TextNoteType.LookupParameter("Tab Size").AsDouble()*304.8
+			# TEXT SIZE
+			dic_text[str(t.Id)]["TextNoteTextSize"] = t.TextNoteType.LookupParameter("Text Size").AsDouble()*304.8
+			# TEXT FONT
+			dic_text[str(t.Id)]["TextNoteFont"] = t.TextNoteType.LookupParameter("Text Font").AsString()
+			# TEXT BOLD
+			dic_text[str(t.Id)]["TextNoteBold"] = t.TextNoteType.LookupParameter("Bold").AsInteger()
+			# TEXT ITALIC
+			dic_text[str(t.Id)]["TextNoteItalic"] = t.TextNoteType.LookupParameter("Italic").AsInteger()
+			# TEXT UNDERLINE
+			dic_text[str(t.Id)]["TextNoteUnderline"] = t.TextNoteType.LookupParameter("Underline").AsInteger()
+			# TEXT WIDTH FACTOR
+			dic_text[str(t.Id)]["TextNoteWidthFactor"] = t.TextNoteType.LookupParameter("Width Factor").AsDouble()
+		except Exception as ex:
+			debugger.append(ex)
+			pass
+	return dic_text
 #----------------------------------------------------#
 #----------------------------------------------------#
 #----------------------------------------------------#
+debugger = []
+
+
 doc = DocumentManager.Instance.CurrentDBDocument
 uiapp = DocumentManager.Instance.CurrentUIApplication
 app = uiapp.Application
@@ -52,24 +91,16 @@ dic = {	"a":{"d10":10,"d16":16,"d20":20},
 		"b":{"d10":11,"d16":17,"d20":21},
 		"c":{"d10":12,"d16":18,"d20":22}}
 
+# TEXTNOTE TYPE
+text_type = None
 
-# Place your code below this line
+
+# GET VIEW SHEET
 view_  = get_sheet_view_by_search_string(view_name)
-text_collector = list(FilteredElementCollector(doc,view_.Id).OfClass(TextNote))
-dic_text = {}
+
+# GET TEXTNOTE
+dic_text = get_textnote(view_)
 
 
-text_type = ""
 
-
-for t in text_collector:
-    try:
-	    dic_text[str(t.Id)] = []
-	    dic_text[str(t.Id)].append(t.Text.strip().split("\t"))#GetFormattedText().GetPlainText().strip()	    
-	    text_type_border = t.TextNoteType.LookupParameter("Leader/Border Offset").AsDouble()*304.8
-	    dic_text[str(t.Id)].append("{:0.0f}".format(round(t.Width*304.8 + 2*text_type_border)))
-    
-    except:
-    	pass
-# Assign your output to the OUT variable.
-OUT = dic_text
+OUT = view_,text_type,dic_text
--- excel_IO/Excel_IO.py
+++ excel_IO/Excel_IO.py
@@ -2,37 +2,47 @@ import clr, time
 clr.AddReference("Microsoft.Office.Interop.Excel")
 # from System.Runtime.InteropServices import Marshal
 import Microsoft.Office.Interop.Excel as Excel
+#---------------------------------------------------------------------------------------#
+#---------------------------------------------------------------------------------------#
+#---------------------------------------------------------------------------------------#
+def write_excel(excel_path,sheet_name,row,column,data):
+    global debugger
+    excel_app = Excel.ApplicationClass()
+    excel_app.Visible = show_excel    
+    workbook = excel_app.Workbooks.Open(excel_path)
+    worksheets = workbook.Worksheets
+    sheet = [worksheets(i+1) for i in range(worksheets.Count) if worksheets(i+1).Name == sheet_name][0]
+    
+    if data.__class__.__name__ == "str":
+        sheet.Cells(row,column).Value = data   
+    
+    # workbook.Close(True,excel_path)
+    excel_app.Save(excel_path)
+    
+    
 
 excel_path = IN[0]
 sheet_name = IN[1]
+show_excel = IN[2]
 
 debugger = []
 
-# Excel app đã khởi tạo + chạy ngầm
-excel_app = Excel.ApplicationClass()
-# Hiện Excel
-excel_app.Visible = False
 
-# Duyệt workbooks
-workbook = excel_app.Workbooks
-# Mở 1 file excel cụ thể
-workbook = workbook.Open(excel_path)
-worksheets = workbook.Worksheets
-sheet = [worksheets(i+1) for i in range(worksheets.Count) if worksheets(i+1).Name == sheet_name][0]
+# write excel:
+write_excel(excel_path,sheet_name,10,10,"Data")
 
-row = 7
-column = 2
-dic_data = {}
-for r in range(1,row+1):
-    try:
-        dic_data[str(r)] = [sheet.Cells(r,c).Text for c in range(1,column+1)]
-        # debugger.append(sheet.Cells(r,column).Text)
-    except:
-        pass
+# dic_data = {}
+# for r in range(1,row+1):
+#     try:
+#         dic_data[str(r)] = [sheet.Cells(r,c).Text for c in range(1,column+1)]
+#         # debugger.append(sheet.Cells(r,column).Text)
+#     except:
+#         pass
 
 
+# OUT = dic_data
 
-OUT = dic_data
 
-time.sleep(5)
-excel_app.Quit()
+# if not show_excel:
+# 	time.sleep(5)
+# 	excel_app.Quit()
